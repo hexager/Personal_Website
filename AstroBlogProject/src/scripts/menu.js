f445f77c1f4020a1c39a0962cca11ff79ed51c3a
BuildTools <unconfigured@null.spigotmc.org>
2024-08-17 15:54:48 +0530

    Fixed Theme Control, Added Persistence

--- AstroBlogProject/src/scripts/menu.js
+++ AstroBlogProject/src/scripts/menu.js
@@ -2,4 +2,22 @@ document.addEventListener('astro:page-load', () => {
   document.querySelector('.hamburger').addEventListener('click', () => {
       document.querySelector('.nav-links').classList.toggle('expanded');
     });
+  document.getElementById("themeToggle").addEventListener("click", handleToggleClick);    
+
+let theme = (() => {
+  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+    return localStorage.getItem('theme');
+  }
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
+  }
+    return 'light';
+})();
+console.log(theme)
+
+if (theme === 'light') {
+  document.documentElement.classList.remove('dark');
+} else {
+  document.documentElement.classList.add('dark');
+}
 });
